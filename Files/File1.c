6984860fce9531c682f9fb7aa49085eb0c3427c3
Vitor Ribeiro <Vitorribeiro20022@gmail.com>
2023-12-30 12:00:09 +0000

    update

---
+++ Files/File1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define MAXBUFFER 80
+
+
+int main(){
+    char com[] = "TEMPXXXXXXX"; // cruar template de ficheiro temporario
+    int comfile = mkstemp(com);
+
+    if(comfile == -1){
+        printf("Erro ao gerar ficheiro de comunicacao");
+        return 1;
+    }
+    
+    int id = fork();
+
+    if(id < 0){
+        printf("Erro ao criar fork \n");
+    }
+    else if (id == 0){
+         char mensagem[MAXBUFFER];
+
+        printf("Indique uma frase");
+        fgets(mensagem,MAXBUFFER,stdin);
+
+        int size = strlen(mensagem);
+        mensagem[size - 1] = '\0';
+
+        write(comfile,&size,sizeof(int) * 1);
+        write(comfile,mensagem, sizeof(char) * size);
+
+        close(comfile);
+        unlink(com); 
+
+    }
+    else{
+        printf("Dentro do pai \n");
+        char mensagem[MAXBUFFER];
+        int size;
+
+        int file = open(com,O_RDONLY);
+
+        while(read(file,&size,sizeof(int) * 1 ) != sizeof(int) * 1){
+            sleep(1);
+            }
+
+        read(file,mensagem,sizeof(char) * size);
+
+        printf("Mensagem recebida pelo pai foi %s", mensagem);
+
+        close(file);
+        unlink(com);
+    }
+
+    return 0;
+}
